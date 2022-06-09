a29dabeaa6428e60b8c65bd6c80232cf6f4f54a1
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-09 18:32:52 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "rose";
+
+function upperCaseCustomerName() {
+    customerName ="bob".toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "tune";
+    return leastFavoriteCustomer;
+}
